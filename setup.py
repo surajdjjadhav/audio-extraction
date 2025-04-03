20c91381c9c5ca19e7c6bc6cabe7f19cbc184a50
surajdjjadhav <sj4456046@gmail.com>
2025-04-03 16:44:47 +0530

    suraj deploy

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup , find_packages
 
 
 setup(
-    name= "src",
+    name= "scripts",
     version= "0.0.1",
     author="surajjadhav",
     author_email = "sj4456046@gmail.com",
