2dbcd7a2dba2af79a796edd471fbe8eb9a2864e5
surajdjjadhav <sj4456046@gmail.com>
2025-04-03 17:49:14 +0530

    suraj jadhav testing

--- app.py
+++ app.py
@@ -88,5 +88,5 @@ import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))  # Use Railway's assigned port
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True ,  host="0.0.0.0", port=port)
 
