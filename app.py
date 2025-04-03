9522f26e8deba2f9ab1b658dccbd8a425d45f1df
surajdjjadhav <sj4456046@gmail.com>
2025-04-03 17:39:05 +0530

    testing

--- app.py
+++ app.py
@@ -83,8 +83,10 @@ def index():
 
     return render_template("index.html")
 
+import os
+
 
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))  # Use Railway-assigned port
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 8080))  # Use Railway's assigned port
+    app.run(host="0.0.0.0", port=port)
+
